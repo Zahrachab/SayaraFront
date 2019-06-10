4598366ff075c9e129baed6cccb9df0161c95d2a
zahra <fz_chabanechaouch@esi.dz>
2019-06-10 16:55:07 +0100

    tests unitaires sur les pages gestion versions et gestion modèles

--- src/app/mocks/Version.Service.mock.ts
+++ src/app/mocks/Version.Service.mock.ts
@@ -16,23 +16,98 @@ export class VersionServiceMock {
   getVersions(codeModele): Observable<VersionDetail[]> {
      return Observable.of ([
        {
-         CodeVersion: 'jj',
-         CodeModele: '',
-         NomVersion: 'kk',
-         options: [{
-           CodeOption : '',
-           NomOption: '',
-           Checked: false,
-           rel_ver_opt: {
-             idRelVerOpt: '',
-             CodeVersion: '',
-             CodeOption: '',
+         CodeVersion: '100015',
+         CodeModele: '1',
+         NomVersion: 'GT Line',
+         options: [
+           {
+             CodeOption: '11',
+             NomOption: 'Toit en verre',
+             rel_ver_opt: {
+               idRelVerOpt: '',
+               CodeVersion: '',
+               CodeOption: '',
+             },
+             Checked: true
+           },
+           {
+             CodeOption: '10',
+             NomOption: 'radar avant',
+             rel_ver_opt: {
+               idRelVerOpt: '',
+               CodeVersion: '',
+               CodeOption: '',
+             },
+             Checked: true
+           },
+           {
+             CodeOption: '11111',
+             NomOption: 'Projecteurs Full LED ',
+             rel_ver_opt: {
+               idRelVerOpt: '',
+               CodeVersion: '',
+               CodeOption: '',
+             },
+             Checked: true
            }
-         }],
+         ],
          couleurs: [{
-           CodeCouleur: '',
-           NomCouleur: '',
-           CodeHexa: '',
+           CodeCouleur: '1000',
+           NomCouleur: 'Gris',
+           CodeHexa: '#3456ff',
+           rel_ver_coul: {
+             idRelVerCoul: 'string',
+             CodeVersion: 'string',
+             CodeCouleur: 'string'
+           },
+         }],
+         images: [{
+           idImage: '',
+           CodeImage: '',
+           Type: '',
+           CheminImage: ''
+         }]
+       },
+       {
+         CodeVersion: '89451',
+         CodeModele: '87651',
+         NomVersion: 'RS',
+         options: [
+           {
+             CodeOption: '11',
+             NomOption: 'Toit en verre',
+             rel_ver_opt: {
+               idRelVerOpt: '',
+               CodeVersion: '',
+               CodeOption: '',
+             },
+             Checked: true
+           },
+           {
+             CodeOption: '10',
+             NomOption: 'radar avant',
+             rel_ver_opt: {
+               idRelVerOpt: '',
+               CodeVersion: '',
+               CodeOption: '',
+             },
+             Checked: true
+           },
+           {
+             CodeOption: '11111',
+             NomOption: 'Projecteurs Full LED ',
+             rel_ver_opt: {
+               idRelVerOpt: '',
+               CodeVersion: '',
+               CodeOption: '',
+             },
+             Checked: true
+           }
+         ],
+         couleurs: [{
+           CodeCouleur: '1000',
+           NomCouleur: 'Gris',
+           CodeHexa: '#3456ff',
            rel_ver_coul: {
              idRelVerCoul: 'string',
              CodeVersion: 'string',
@@ -46,8 +121,7 @@ export class VersionServiceMock {
            CheminImage: ''
          }]
        }
-       ]
-     );
+       ]);
   }
 
   supprimerVersion(codeVersion) {
--- src/app/pages/gestion/gestion-version/gestion-version.component.spec.ts
+++ src/app/pages/gestion/gestion-version/gestion-version.component.spec.ts
@@ -1,14 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GestionVersionComponent } from './gestion-version.component';
+import {VersionServiceMock} from '../../../mocks/Version.Service.mock';
+import {VersionDetail} from '../../../services/entites/versionDetail.model';
+import {HttpClientModule} from '@angular/common/http';
+import {
+  MatDialog,
+  MatInputModule,
+  MatPaginatorModule,
+  MatSelectModule,
+  MatTableModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ModeleService} from '../../../services/modele.service';
+import {ModeleServiceMock} from '../../../mocks/Modele.Service.mock';
+import {OptionService} from '../../../services/option.service';
+import {OptionServiceMock} from '../../../mocks/Option.Service.mock';
+import {CouleurService} from '../../../services/couleur.service';
+import {CouleurServiceMock} from '../../../mocks/Couleur.Service.mock';
+import {VersionService} from '../../../services/version.service';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
-describe('GestionVersionComponent', () => {
+fdescribe('GestionVersionComponent', () => {
   let component: GestionVersionComponent;
   let fixture: ComponentFixture<GestionVersionComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GestionVersionComponent ]
+      imports: [HttpClientModule, MatTableModule, MatPaginatorModule, BrowserAnimationsModule, MatSelectModule, MatInputModule],
+      declarations: [ GestionVersionComponent ],
+      providers: [
+        {provide : ModeleService , useClass : ModeleServiceMock},
+        {provide : OptionService , useClass : OptionServiceMock},
+        {provide : CouleurService , useClass : CouleurServiceMock},
+        {provide : VersionService , useClass : VersionServiceMock},
+        {provide: MatDialog, useValue: {} }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -22,4 +50,61 @@ describe('GestionVersionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  /*Tester si le tableau est bien affiché avec des données renvoyées par le service VersionServiceMock */
+  it('should test the table ', () => {
+    // Le mock du service VersionService
+    const versionService = new VersionServiceMock();
+    // La liste des versions renvoyée par VersionServiceMock
+    let data: VersionDetail[];
+    versionService.getVersions(1).subscribe(res => {
+      data = res as VersionDetail[];
+    });
+    fixture.detectChanges();
+    // tester le clique sur le mat-select
+    const trigger = fixture.debugElement.query(By.css('.mat-select-trigger')).nativeElement;
+    trigger.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      // tester le clique sur le premier modèle qui apparait dans le mat-select
+      const matOption = fixture.debugElement.queryAll(By.css('.mat-option'));
+      matOption[1].nativeElement.click();
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        // tester l'affichage du tableau après le clique sur un modèle donnée
+        const tableRows = fixture.nativeElement.querySelectorAll('mat-row');
+        // tester le nombre de ligne dans le tableau
+        expect(tableRows.length).toBe(data.length);
+        // tester les header
+        const headerRow = fixture.nativeElement.querySelectorAll('mat-header-cell');
+        expect(headerRow[0].innerHTML).toContain('CODE');
+        expect(headerRow[1].innerHTML).toContain('VERSION');
+        expect(headerRow[2].innerHTML).toContain('MODELE');
+        expect(headerRow[3].innerHTML).toContain('OPTIONS');
+        expect(headerRow[4].innerHTML).toContain('GESTION');
+        let i = 0;
+        for (const row of data) {
+          // tester l'affichage des données sur le tableau
+          const row1 = tableRows[i++];
+          expect(row1.childNodes[1].innerHTML).toContain(row.CodeVersion);
+          expect(row1.childNodes[2].innerHTML).toContain(row.NomVersion);
+          expect(row1.childNodes[3].innerHTML).toContain(row.CodeModele);
+          // Tester que les versions d'un modèle sont bien affichés
+          for (const option of row.options) {
+            expect(row1.childNodes[4].innerHTML).toContain(option.NomOption);
+          }
+        }
+      });
+    });
+  });
+
+  // tester le clique sur le bouton ajouter une nouvelle version
+  it('should test button ajouter Version', async(() => {
+    spyOn(component, 'ajouterVersion');
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    fixture.whenStable().then(() => {
+      expect(component.ajouterVersion).toHaveBeenCalled();
+    });
+  }));
 });
