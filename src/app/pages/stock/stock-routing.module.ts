dfdd2eb9b400c2972d8fed209f2159692d376590
Chouaki Salim <fs_chouaki@esi.dz>
2019-06-23 12:10:06 +0100

    Stock a été transformé en module, et le sous menu a été rajouté

---
+++ src/app/pages/stock/stock-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {StockComponent} from './stock/stock.component';
+import {StockVehiculesComponent} from './stock-vehicules/stock-vehicules.component';
+import {StockUploadComponent} from './stock-upload/stock-upload.component';
+
+const routes: Routes = [
+  {
+    path: 'stock',
+    component: StockComponent,
+    children: [
+      {
+        path: 'vehicules',
+        component: StockVehiculesComponent,
+      },
+      {
+        path: 'importer',
+        component: StockUploadComponent,
+      }
+    ]
+  }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class StockRoutingModule { }
