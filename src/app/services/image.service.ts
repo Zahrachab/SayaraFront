c271a074059aa624eda14f342fdcb1cc8ef3a093
zahra <fz_chabanechaouch@esi.dz>
2019-08-03 22:53:42 +0100

    commenter le service Image

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -1,6 +1,5 @@
 import {Injectable, Injector} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +10,26 @@ export class ImageService {
   constructor(private http: HttpClient, private injector: Injector) {}
 
 
+  /**
+   * Ajouter une photo à une couleur d'une version
+   * @param image
+   * @param codeVersion
+   * @param codeCouleur
+   */
   public uploadImage(image: File, codeVersion: string, codeCouleur: string) {
     const formData = new FormData();
-
     formData.append('imageVersion', image);
     formData.append('CodeCouleur', codeCouleur);
     const tokenHeader = new HttpHeaders().set('Authorization', 'Bearer ' + JSON.parse(localStorage.getItem('utilisateur')).token);
     return this.http.post(this.imagesVersionUrl + codeVersion, formData, {headers: tokenHeader});
   }
+
+
+  /**
+   * Supprimer une photo d'une couleur d'une version
+   * @param id
+   * @param codeVersion
+   */
   public supprimerImage(id: string, codeVersion: string) {
       const tokenHeader = new HttpHeaders().set('Authorization', 'Bearer ' + JSON.parse(localStorage.getItem('utilisateur')).token);
       return this.http.delete(this.url + '/images/' + id,
