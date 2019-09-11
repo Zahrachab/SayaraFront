aa2018239849d97fd662b8dc13b46e622eaf1a8a
zahra <fz_chabanechaouch@esi.dz>
2019-09-11 01:28:08 +0100

    fixer quelques problèmes d'interface pour commandes

---
+++ src/app/mocks/ToastManagerMock.ts
@@ -0,0 +1,19 @@
+
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class ToastManagerMock {
+  constructor() {
+  }
+
+  errorToastr(message: string){
+
+  }
+  successToastr(message: string) {
+  }
+
+  infoToastr(message: string) {
+
+  }
+
+}
